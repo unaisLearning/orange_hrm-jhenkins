0998ec5cb688e35e53490494f39340fa6d5ced41
“unaisLearning” <unais.learning@gmail.com>
2025-06-17 14:13:11 +0530

    webhook implementation test latest

--- pages/base_page.py
+++ pages/base_page.py
@@ -201,4 +201,4 @@ class BasePage:
         self.driver.save_screenshot(filepath)
         logger.info(f"Screenshot saved: {filepath}")
         return filepath 
-    #Test again
+    #Test again again
